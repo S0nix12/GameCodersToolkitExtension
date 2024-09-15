3b7425e450c860927271f237082675feb2151242
S0nix <henning-ax@web.de>
2024-09-15 21:03:50 +0200

    Set Extension version to 0.1.1

--- source.extension.cs
+++ source.extension.cs
@@ -11,7 +11,7 @@ namespace GameCodersToolkit
         public const string Name = "GameCodersToolkit";
         public const string Description = @"Find references of your code in data files.";
         public const string Language = "en-US";
-        public const string Version = "1.0";
+        public const string Version = "0.1.1";
         public const string Author = "Henning Ax";
         public const string Tags = "";
     }
