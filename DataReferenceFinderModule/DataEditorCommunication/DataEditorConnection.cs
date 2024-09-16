1318903864cb18375d99426c708557f97690d5ce
S0nix <henning-ax@web.de>
2024-09-17 00:08:40 +0200

    Change Websocket implementation from WatsonWebsocket to using a custom implementation

--- DataReferenceFinderModule/DataEditorCommunication/DataEditorConnection.cs
+++ DataReferenceFinderModule/DataEditorCommunication/DataEditorConnection.cs
@@ -1,11 +1,13 @@
-﻿using GameCodersToolkit.DataReferenceFinderModule.ReferenceDatabase;
+﻿using EnvDTE;
+using GameCodersToolkit.DataReferenceFinderModule.ReferenceDatabase;
 using GameCodersToolkit.Utils;
 using Microsoft.VisualStudio.Threading;
+using System.Net.Sockets;
 using System.Net.WebSockets;
+using System.Text;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
-using WatsonWebsocket;
 
 namespace GameCodersToolkit.DataReferenceFinderModule.DataEditorCommunication
 {
@@ -15,116 +17,230 @@ namespace GameCodersToolkit.DataReferenceFinderModule.DataEditorCommunication
 		public bool IsConnected { get; set; }
 	}
 
+	public enum ESocketStatus
+	{
+		Inactive,
+		Connecting,
+		Open
+	}
+
 	public class DataEditorClientSocket : IDisposable
 	{
-		public DataEditorClientSocket(string serverAddress)
+		public DataEditorClientSocket()
 		{
-			m_socket = new WatsonWsClient(new Uri(serverAddress));
-			m_socket.ConfigureOptions(options => options.AddSubProtocol("GameCodersToolkit.DataReferenceFinder"));
-			m_socket.ServerDisconnected += ServerDisconnected;
-			m_socket.ServerConnected += ServerConnected;
-			ServerAddress = serverAddress;
+
 		}
 
-		public void StartBackgroundAutoConnection(bool restartWhenClosed)
+		public void Dispose()
 		{
+			m_tryReconnect = false;
+			m_pendingConnectionCancellationSource?.Cancel();
+			m_reciveLoopCancellationSource?.Cancel();
+
+			m_pendingConnectionLoop?.Wait();
+			m_receiveLoop?.Wait();
+			m_socket?.Dispose();
+			SocketStatus = ESocketStatus.Inactive;
+		}
+
+		public async Task StartAsync(Uri address, bool tryReconnect)
+		{
+			if (SocketStatus != ESocketStatus.Inactive)
+			{
+				await StopAsync();
+			}
+
 			lock (m_mutex)
 			{
-				m_restartConnectionWhenClosed = restartWhenClosed;
-				if (IsAutoConnecting)
-					return;
+				m_tryReconnect = tryReconnect;
+				m_requestedAddress = address;
+				m_socket = new ClientWebSocket();
+				m_socket.Options.AddSubProtocol("GameCodersToolkit.DataReferenceFinder");
+				SocketStatus = ESocketStatus.Connecting;
 
-				m_autoConnectionTask = Task.Run(() => AutoConnectionAsync(m_autoConnectionCancelSource.Token), m_autoConnectionCancelSource.Token);
-			}
-		}
-
-		public async Task StartSocketAsync()
-		{
-			if (!m_socket.Connected)
-			{
-				await m_socket.StartAsync();
+				m_pendingConnectionCancellationSource = new CancellationTokenSource();
+				m_pendingConnectionLoop = Task.Run(PendingConnectLoopAsync).ContinueWith(StartReceiveLoop, TaskScheduler.Default);
 			}
 		}
 
 		public async Task SendMessageToDataEditorAsync<TValue>(TValue jsonObject)
 		{
-			if (!m_socket.Connected)
+			if (SocketStatus != ESocketStatus.Open)
 			{
-				await m_socket.StartAsync();
-				if (!m_socket.Connected)
-				{
-					await GameCodersToolkitPackage.ExtensionOutput.WriteLineAsync("Could not send Message to DataEditor. No connection possible");
-					return;
-				}
+				return;
 			}
 
 			var options = new JsonSerializerOptions { WriteIndented = true };
 
 			byte[] jsonBytes = JsonSerializer.SerializeToUtf8Bytes(jsonObject, options);
-			await m_socket.SendAsync(new ArraySegment<byte>(jsonBytes), WebSocketMessageType.Text, CancellationToken.None);
+			await m_socket.SendAsync(new ArraySegment<byte>(jsonBytes), WebSocketMessageType.Text, true, CancellationToken.None);
 		}
 
-		public async Task CloseDataEditorConnectionAsync()
+		public async Task StopAsync()
 		{
-			m_autoConnectionCancelSource.Cancel();
-			m_restartConnectionWhenClosed = false;
-			await m_socket.StopAsync();
-		}
-
-		public void Dispose()
-		{
-			m_autoConnectionCancelSource.Cancel();
-			if (m_socket.Connected)
+			lock (m_mutex)
 			{
-				m_socket.Stop();
+				m_tryReconnect = false;
+				m_pendingConnectionCancellationSource?.Cancel();
 			}
-			m_socket.Dispose();
-		}
 
-		async Task AutoConnectionAsync(CancellationToken cancelToken)
-		{
-			while (!m_socket.Connected && !cancelToken.IsCancellationRequested)
+			if (SocketStatus == ESocketStatus.Open)
 			{
-				if (await m_socket.StartWithTimeoutAsync(token: cancelToken))
+				var timeout = new CancellationTokenSource(2000);
+				try
 				{
-					break;
+					// after this, the socket state which change to CloseSent
+					await m_socket.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "Closing", timeout.Token);
+					// now we wait for the server response, which will close the socket
+					while (m_socket.State != WebSocketState.Closed && !timeout.Token.IsCancellationRequested) ;
+				}
+				catch (OperationCanceledException)
+				{
+
+				}
+				catch (Exception ex)
+				{
+					await DiagnosticUtils.ReportExceptionFromExtensionAsync(
+						"DataEditorSocket Exception while stopping socket",
+						ex);
 				}
 
-				await Task.Delay(2000);
+				lock (m_mutex)
+				{
+					m_reciveLoopCancellationSource?.Cancel();
+				}
+				await Task.Delay(50);
 			}
 
 			lock (m_mutex)
 			{
-				m_autoConnectionTask = null;
+				m_socket?.Dispose();
+				m_socket = null;
+				SocketStatus = ESocketStatus.Inactive;
 			}
 		}
 
-		void ServerConnected(object sender, EventArgs e)
+		private async Task ReceiveLoopAsync()
 		{
-			ConnectionStatusChanged?.Invoke(this, new ConnectionStatusChangedEventArgs(true));
-		}
-
-		void ServerDisconnected(object sender, EventArgs args)
-		{
-			if (m_restartConnectionWhenClosed)
+			var cancellationToken = m_reciveLoopCancellationSource.Token;
+			try
 			{
-				StartBackgroundAutoConnection(m_restartConnectionWhenClosed);
+				var buffer = WebSocket.CreateClientBuffer(4096, 4096);
+				while (m_socket.State != WebSocketState.Closed && !cancellationToken.IsCancellationRequested)
+				{
+					var receiveResult = await m_socket.ReceiveAsync(buffer, cancellationToken);
+					// if the token is cancelled while ReceiveAsync is blocking, the socket state changes to aborted and it can't be used
+					if (!cancellationToken.IsCancellationRequested)
+					{
+						// the server is notifying us that the connection will close; send acknowledgement
+						if (m_socket.State == WebSocketState.CloseReceived && receiveResult.MessageType == WebSocketMessageType.Close)
+						{
+							await m_socket.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "Acknowledge Close frame", CancellationToken.None);
+						}
+
+						// Nothing else is interesting for us. We only use the Socket for sending;
+					}
+				}
 			}
-			ConnectionStatusChanged?.Invoke(this, new ConnectionStatusChangedEventArgs(false));
+			catch (OperationCanceledException)
+			{
+				// normal upon task/token cancellation, disregard
+			}
+			catch (Exception ex)
+			{
+				await DiagnosticUtils.ReportExceptionFromExtensionAsync(
+					"DataEditorSocket Exception during Receive Loop",
+					ex);
+			}
+			finally
+			{
+				lock (m_mutex)
+				{
+					m_socket.Dispose();
+					m_socket = null;
+					SocketStatus = ESocketStatus.Inactive;
+				}
+			}
 		}
 
-		public EventHandler<ConnectionStatusChangedEventArgs> ConnectionStatusChanged { get; set; }
-		public bool IsConnected { get => m_socket.Connected; }
-		public bool IsAutoConnecting { get => m_autoConnectionTask != null; }
-		public string ServerAddress { get; private set; }
+		private async Task PendingConnectLoopAsync()
+		{
+			var cancellationToken = m_pendingConnectionCancellationSource.Token;
+			try
+			{
+				while (m_socket.State != WebSocketState.Open && !cancellationToken.IsCancellationRequested)
+				{
+					await Task.Delay(2000, cancellationToken);
+					if (m_socket.State == WebSocketState.Open || cancellationToken.IsCancellationRequested)
+					{
+						break;
+					}
 
-		CancellationTokenSource m_autoConnectionCancelSource = new CancellationTokenSource();
+					try
+					{
+						await m_socket.ConnectAsync(m_requestedAddress, cancellationToken);
+					}
+					catch (WebSocketException)
+					{
+						// Retry connection, the exception results in our socket being disposed so create a new one
+						m_socket = new ClientWebSocket();
+						m_socket.Options.AddSubProtocol("GameCodersToolkit.DataReferenceFinder");
+					}
+				}
 
-		Task m_autoConnectionTask = null;
-		bool m_restartConnectionWhenClosed = false;
-		WatsonWsClient m_socket;
+				lock (m_mutex)
+				{
+					if (!cancellationToken.IsCancellationRequested && m_socket.State == WebSocketState.Open)
+					{
+						SocketStatus = ESocketStatus.Open;
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				await DiagnosticUtils.ReportExceptionFromExtensionAsync(
+					"DataEditorSocket Exception during Connect Loop",
+					ex);
+			}
+		}
 
-		object m_mutex = new();
+		private void StartReceiveLoop(Task connectionTask)
+		{
+			if (m_socket != null && SocketStatus == ESocketStatus.Open && (m_reciveLoopCancellationSource == null || !m_reciveLoopCancellationSource.IsCancellationRequested))
+			{
+				m_reciveLoopCancellationSource = new CancellationTokenSource();
+				m_receiveLoop = Task.Run(ReceiveLoopAsync).ContinueWith(RestartPendingConnectionLoop, TaskScheduler.Default);
+			}
+		}
+
+		private void RestartPendingConnectionLoop(Task receiveTask)
+		{
+			if (!m_tryReconnect || m_pendingConnectionCancellationSource.IsCancellationRequested)
+				return;
+
+			m_socket = new ClientWebSocket();
+			m_socket.Options.AddSubProtocol("GameCodersToolkit.DataReferenceFinder");
+			SocketStatus = ESocketStatus.Connecting;
+
+			m_pendingConnectionCancellationSource = new CancellationTokenSource();
+			m_pendingConnectionLoop = Task.Run(PendingConnectLoopAsync).ContinueWith(StartReceiveLoop, TaskScheduler.Default);
+		}
+
+		public ESocketStatus SocketStatus { get; private set; }
+
+		Uri m_requestedAddress;
+		bool m_tryReconnect = false;
+
+		ClientWebSocket m_socket;
+		ClientWebSocketOptions m_options;
+
+		Task m_receiveLoop;
+		CancellationTokenSource m_reciveLoopCancellationSource;
+
+		Task m_pendingConnectionLoop;
+		CancellationTokenSource m_pendingConnectionCancellationSource;
+
+		object m_mutex = new object();
 	}
 
 	public class DataEditorConnection
@@ -132,13 +248,12 @@ namespace GameCodersToolkit.DataReferenceFinderModule.DataEditorCommunication
 		public DataEditorConnection()
 		{
 			GameCodersToolkitPackage.DataLocationsConfig.ConfigLoaded += OnConfigLoaded;
-			CreateSocket();
-			m_clientSocket?.StartBackgroundAutoConnection(true);
+			m_clientSocket = new DataEditorClientSocket();
 		}
 
 		public async Task OpenInDataEditorAsync(DataEntry dataEntry)
 		{
-			if (m_clientSocket == null || !m_clientSocket.IsConnected)
+			if (m_clientSocket.SocketStatus != ESocketStatus.Open)
 				return;
 
 			OpenDataEntryMessage message = new OpenDataEntryMessage(dataEntry);
@@ -155,39 +270,16 @@ namespace GameCodersToolkit.DataReferenceFinderModule.DataEditorCommunication
 			}
 		}
 
-		void CreateSocket()
-		{
-			try
-			{
-				bool wasConnected = IsConnectedToDataEditor;
-				m_clientSocket?.Dispose();
-				if (wasConnected)
-				{
-					DataEditorConnectionStatusChanged?.Invoke(this, new ConnectionStatusChangedEventArgs(false));
-				}
-
-				string serverAddress = GameCodersToolkitPackage.DataLocationsConfig.GetDataEditorServerUri();
-				if (!string.IsNullOrWhiteSpace(serverAddress))
-				{
-					m_clientSocket = new DataEditorClientSocket(serverAddress);
-					m_clientSocket.ConnectionStatusChanged += OnConnectionStatusChanged;
-					m_clientSocket?.StartBackgroundAutoConnection(true);
-				}
-			}
-			catch (Exception ex)
-			{
-				ThreadHelper.JoinableTaskFactory.Run(() => DiagnosticUtils.ReportExceptionFromExtensionAsync(
-					"Failed to create DateEditorConnection Socket",
-					ex));
-			}
-		}
-
 		void OnConfigLoaded(object sender, EventArgs e)
 		{
 			string serverAddress = GameCodersToolkitPackage.DataLocationsConfig.GetDataEditorServerUri();
-			if (m_clientSocket == null || m_clientSocket.ServerAddress != serverAddress)
+			if (Uri.TryCreate(serverAddress, UriKind.RelativeOrAbsolute, out Uri serverUri))
 			{
-				CreateSocket();
+				ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
+				{
+					await m_clientSocket.StopAsync();
+					await m_clientSocket.StartAsync(serverUri, true);
+				}).FireAndForget();
 			}
 		}
 
@@ -197,8 +289,8 @@ namespace GameCodersToolkit.DataReferenceFinderModule.DataEditorCommunication
 		}
 
 		public EventHandler<ConnectionStatusChangedEventArgs> DataEditorConnectionStatusChanged { get; set; }
-		public bool IsConnectedToDataEditor { get => m_clientSocket != null ? m_clientSocket.IsConnected : false; }
+		public bool IsConnectedToDataEditor { get => m_clientSocket.SocketStatus == ESocketStatus.Open; }
 
-		static DataEditorClientSocket? m_clientSocket;
+		DataEditorClientSocket m_clientSocket;
 	}
 }
