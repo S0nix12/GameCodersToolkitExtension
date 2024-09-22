a0be29cd870218885050baebce86f893f7e66184
S0nix <henning-ax@web.de>
2024-09-22 21:05:26 +0200

    Created basic Code Lens project setup and test data point

---
+++ DataReferenceCodeLensProviderShared/CodeLensDataPoint.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.Language.CodeLens;
+using Microsoft.VisualStudio.Language.CodeLens.Remoting;
+using Microsoft.VisualStudio.Threading;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DataReferenceCodeLensProviderShared
+{
+	public class CodeLensDataPoint : IAsyncCodeLensDataPoint
+	{
+		public CodeLensDataPoint(CodeLensDescriptor descriptor)
+		{
+			Descriptor = descriptor;
+		}
+
+		public Task<CodeLensDataPointDescriptor> GetDataAsync(CodeLensDescriptorContext descriptorContext, CancellationToken token)
+		{
+			return Task.FromResult(new CodeLensDataPointDescriptor
+			{
+				Description = "Test Code Lens Description",
+				TooltipText = "Test Tooltip Text"
+			});
+		}
+
+		public Task<CodeLensDetailsDescriptor> GetDetailsAsync(CodeLensDescriptorContext descriptorContext, CancellationToken token)
+		{
+			return Task.FromResult<CodeLensDetailsDescriptor>(null);
+		}
+
+		public CodeLensDescriptor Descriptor { get; }
+		public event AsyncEventHandler InvalidatedAsync;
+	}
+
+}
---
+++ DataReferenceCodeLensProviderShared/CodeLensDataPointProvider.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.Language.CodeLens.Remoting;
+using Microsoft.VisualStudio.Language.CodeLens;
+using Microsoft.VisualStudio.Language.Intellisense;
+using System;
+using System.Threading.Tasks;
+using System.Threading;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Utilities;
+using DataReferenceCodeLensProvider.Properties;
+
+namespace DataReferenceCodeLensProviderShared
+{
+	[Export(typeof(IAsyncCodeLensDataPointProvider))]
+	[Name(Id)]
+	[ContentType("C/C++")]
+	[LocalizedName(typeof(Resources), Id)]
+	[Priority(210)]
+	public class CodeLensDataPointProvider : IAsyncCodeLensDataPointProvider
+	{
+		public Task<bool> CanCreateDataPointAsync(CodeLensDescriptor descriptor, CodeLensDescriptorContext context, CancellationToken token)
+		{
+			var methodsOnly = descriptor.Kind == CodeElementKinds.Method || descriptor.Kind == CodeElementKinds.Function;
+			return Task.FromResult(methodsOnly);
+		}
+
+		public Task<IAsyncCodeLensDataPoint> CreateDataPointAsync(CodeLensDescriptor descriptor, CodeLensDescriptorContext descriptorContext, CancellationToken token)
+		{
+			return Task.FromResult<IAsyncCodeLensDataPoint>(new CodeLensDataPoint(descriptor));
+		}
+
+
+		internal const string Id = "DataReferenceCodeLensProvider";
+	}
+
+}
