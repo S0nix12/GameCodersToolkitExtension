878f9842c144768bce8c2bb50a18dc85c31a5942
S0nix <henning-ax@web.de>
2024-09-28 12:40:22 +0200

    Cleanup Code Lens Tagger and move parsing into smaller functions

--- GameCodersToolkitShared/DataReferenceFinderModule/CodeLensTagging/CodeLensTagger.cs
+++ GameCodersToolkitShared/DataReferenceFinderModule/CodeLensTagging/CodeLensTagger.cs
@@ -1,25 +1,14 @@
-﻿using Community.VisualStudio.Toolkit;
-using GameCodersToolkit;
+﻿using GameCodersToolkit;
 using GameCodersToolkit.ReferenceFinder;
-using GameCodersToolkit.Utils;
 using Microsoft.VisualStudio.Language.CodeLens;
 using Microsoft.VisualStudio.Language.Intellisense;
-using Microsoft.VisualStudio.PlatformUI;
-using Microsoft.VisualStudio.RpcContracts.Build;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Tagging;
-using Microsoft.VisualStudio.Threading;
 using Microsoft.VisualStudio.Utilities;
-using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using System.Windows.Navigation;
 
 namespace GameCodersToolkitShared.DataReferenceFinderModule.CodeLensTagging
 {
@@ -27,6 +16,33 @@ namespace GameCodersToolkitShared.DataReferenceFinderModule.CodeLensTagging
 	enum CustomCodeElementKinds
 	{
 		DataReference = 1 << 24
+	}
+
+	class ChangedLineTracker
+	{
+		public void UpdateLine(int lineNumber)
+		{
+			FirstChangedLine = Math.Min(FirstChangedLine, lineNumber);
+			LastChangedLine = Math.Max(LastChangedLine, lineNumber);
+		}
+
+		public int FirstChangedLine { get; private set; } = int.MaxValue;
+		public int LastChangedLine { get; private set; } = -1;
+	}
+
+	struct ParseResultLine
+	{
+		public int LineNumber;
+		public string GuidString;
+		public string Identifier;
+	}
+
+	class ParseResultLineComparer : IComparer<ParseResultLine>
+	{
+		public int Compare(ParseResultLine a, ParseResultLine b)
+		{
+			return a.LineNumber - b.LineNumber;
+		}
 	}
 
 	[Export(typeof(ITaggerProvider))]
@@ -49,7 +65,7 @@ namespace GameCodersToolkitShared.DataReferenceFinderModule.CodeLensTagging
 			Properties = new CodeLensTagProperties(true);
 		}
 
-		public ICodeLensDescriptor Descriptor {  get; set; }
+		public ICodeLensDescriptor Descriptor { get; set; }
 
 		public CodeLensTagProperties Properties { get; set; }
 
@@ -69,15 +85,15 @@ namespace GameCodersToolkitShared.DataReferenceFinderModule.CodeLensTagging
 
 	public class DataReferenceCodeLensDescriptor : ICodeLensDescriptor
 	{
-		public string FilePath {  get; set; }
+		public string FilePath { get; set; }
 
-		public Guid ProjectGuid {  get; set; }
+		public Guid ProjectGuid { get; set; }
 
-		public string ElementDescription {  get; set; }
+		public string ElementDescription { get; set; }
 
-		public Span? ApplicableSpan {  get; set; }
+		public Span? ApplicableSpan { get; set; }
 
-		public CodeElementKinds Kind {  get; set; }
+		public CodeElementKinds Kind { get; set; }
 	}
 
 	internal sealed class CodeLensTagger : ITagger<ICodeLensTag>
@@ -110,6 +126,18 @@ namespace GameCodersToolkitShared.DataReferenceFinderModule.CodeLensTagging
 
 		public event EventHandler<SnapshotSpanEventArgs> TagsChanged;
 
+		private DataReferenceCodeLensDescriptor GetCodeLensDescriptorForSpan(SnapshotSpan snapshotSpan)
+		{
+			return new DataReferenceCodeLensDescriptor
+			{
+				ElementDescription = "DataReference",
+				FilePath = m_textBuffer.GetFileName(),
+				Kind = (CodeElementKinds)CustomCodeElementKinds.DataReference,
+				ProjectGuid = Guid.Empty,
+				ApplicableSpan = snapshotSpan
+			};
+		}
+
 		public IEnumerable<ITagSpan<ICodeLensTag>> GetTags(NormalizedSnapshotSpanCollection spans)
 		{
 			if (spans.Count == 0)
@@ -118,7 +146,7 @@ namespace GameCodersToolkitShared.DataReferenceFinderModule.CodeLensTagging
 			SnapshotSpan entire = new SnapshotSpan(spans[0].Start, spans[spans.Count - 1].End).TranslateTo(m_currentSnapshot, SpanTrackingMode.EdgeExclusive);
 			int startLineNumber = entire.Start.GetContainingLine().LineNumber;
 			int endLineNumber = entire.End.GetContainingLine().LineNumber;
-			
+
 			foreach (var guidResultLine in m_guidResultLines)
 			{
 				if (guidResultLine.LineNumber > endLineNumber)
@@ -133,16 +161,8 @@ namespace GameCodersToolkitShared.DataReferenceFinderModule.CodeLensTagging
 
 				DataReferenceCodeLensTag codeLensTag = new DataReferenceCodeLensTag();
 				codeLensTag.DataReferenceIdentifier = guidResultLine.GuidString;
-				SnapshotSpan tagSpan = new SnapshotSpan(lineSnapshot.Start,
-					lineSnapshot.End);
-				codeLensTag.Descriptor = new DataReferenceCodeLensDescriptor
-				{
-					ElementDescription = "DataReference",
-					FilePath = m_textBuffer.GetFileName(),
-					Kind = (CodeElementKinds)CustomCodeElementKinds.DataReference,
-					ProjectGuid = Guid.Empty,
-					ApplicableSpan = tagSpan
-				};
+				SnapshotSpan tagSpan = new SnapshotSpan(lineSnapshot.Start, lineSnapshot.End);
+				codeLensTag.Descriptor = GetCodeLensDescriptorForSpan(tagSpan);
 
 				yield return new TagSpan<ICodeLensTag>(tagSpan, codeLensTag);
 			}
@@ -168,48 +188,105 @@ namespace GameCodersToolkitShared.DataReferenceFinderModule.CodeLensTagging
 			ChangedLineTracker changeTracker = new ChangedLineTracker();
 
 			// Remove all lines we found in the old span
-			SnapshotSpan oldSnapshotSpan = new SnapshotSpan(oldSnapshot, oldSpan);
-			int oldStartLine = oldSnapshotSpan.Start.GetContainingLine().LineNumber;
-			int oldEndLine = oldSnapshotSpan.End.GetContainingLine().LineNumber;
-
-			Span guidResultsToRemove = GetRangeToRemove(oldStartLine, oldEndLine, ref m_guidResultLines);
-			Span removedGuidLinesSpan = new Span();
-			if (!guidResultsToRemove.IsEmpty)
-			{
-				removedGuidLinesSpan = new Span(m_guidResultLines[guidResultsToRemove.Start].LineNumber, m_guidResultLines[guidResultsToRemove.End - 1].LineNumber);
-				m_guidResultLines.RemoveRange(guidResultsToRemove.Start, guidResultsToRemove.Length);
-			}
-
-			Span identifierDefsToRemove = GetRangeToRemove(oldStartLine, oldEndLine, ref m_identifierDefinitionLines);
-			List<string> identifierToReparse = new List<string>();
-			for (int i = identifierDefsToRemove.Start; i < identifierDefsToRemove.End; ++i)
-			{
-				identifierToReparse.Add(m_identifierDefinitionLines[i].Identifier);
-			}
-			m_identifierDefinitionLines.RemoveRange(identifierDefsToRemove.Start, identifierDefsToRemove.Length);
-
-
-			List<string> guidFieldIdentifiers = new List<string>();
-			guidFieldIdentifiers = GameCodersToolkitPackage.DataLocationsConfig.GetGuidFieldIdentifiers();
-
-			Dictionary<string, string> identifierToGuid = new Dictionary<string, string>();
-			Dictionary<string, List<int>> identifiersToResolve = new Dictionary<string, List<int>>();
-
-			SnapshotSpan newSnapshotSpan = new SnapshotSpan(newSnapshot, newSpan);
-			int newStartLine = newSnapshotSpan.Start.GetContainingLine().LineNumber;
-			int newEndLine = newSnapshotSpan.End.GetContainingLine().LineNumber;
-
-			if (!removedGuidLinesSpan.IsEmpty)
-			{
-				changeTracker.UpdateLine(removedGuidLinesSpan.Start);
-				changeTracker.UpdateLine(removedGuidLinesSpan.End);
-			}
+			List<string> identifierToReparse = RemoveChangedLineEntries(oldSnapshot, oldSpan, changeTracker);
 
 			int guidResultCountBefore = m_guidResultLines.Count;
+
+			SnapshotSpan newSnapshotSpan = new SnapshotSpan(newSnapshot, newSpan);
+			ParseLinesInSnapshotSpan(newSnapshotSpan, changeTracker, out Dictionary<string, List<int>> identifiersToResolve);
+			ReparseChangedIdentifier(identifierToReparse, guidResultCountBefore, identifiersToResolve, changeTracker);
+
+			// TODO resolve remaining guid identifiers with corresponding file
+
+			// Offset all lines we did not reparse but come after the new text span by the line delta of the change so our results stay attached to the correct line
+			ApplyLineChangeDelta(lineDelta, newSnapshotSpan.Start.GetContainingLine().LineNumber, guidResultCountBefore, changeTracker);
+
+			// Sort all results by their line number again.
+			ParseResultLineComparer resultLineComparer = new ParseResultLineComparer();
+			m_guidResultLines.Sort(resultLineComparer);
+			m_identifierDefinitionLines.Sort(resultLineComparer);
+
+			stopwatch.Stop();
+			GameCodersToolkitPackage.ExtensionOutput?.WriteLine($"Parsing Text Buffer {m_textBuffer.GetFileName()} took {stopwatch.ElapsedMilliseconds}ms");
+
+			m_currentSnapshot = newSnapshot;
+			// We changed atleast one line. Make sure to notify about it
+			if (changeTracker.FirstChangedLine != int.MaxValue)
+			{
+				ITextSnapshotLine firstSnapshotLine = newSnapshot.GetLineFromLineNumber(changeTracker.FirstChangedLine);
+				ITextSnapshotLine lastSnapshotLine = newSnapshot.GetLineFromLineNumber(changeTracker.LastChangedLine);
+				TagsChanged?.Invoke(this,
+					new SnapshotSpanEventArgs(
+						new SnapshotSpan(newSnapshot, firstSnapshotLine.Start, lastSnapshotLine.End)));
+			}
+		}
+
+		// Apply the line delta of a change to current results
+		private void ApplyLineChangeDelta(int lineDelta, int changeStartLine, int guidResultCountBefore, ChangedLineTracker changeTracker)
+		{
+			if (lineDelta != 0)
+			{
+				for (int i = 0; i < guidResultCountBefore; ++i)
+				{
+					if (m_guidResultLines[i].LineNumber >= changeStartLine)
+					{
+						ParseResultLine parseResultLine = m_guidResultLines[i];
+						changeTracker.UpdateLine(parseResultLine.LineNumber);
+
+						parseResultLine.LineNumber += lineDelta;
+						m_guidResultLines[i] = parseResultLine;
+
+						changeTracker.UpdateLine(parseResultLine.LineNumber);
+					}
+				}
+			}
+		}
+
+		// Adjust all old guid results for which the identifier was potentially modified with a potentially changed guid
+		// Remove all old guid results for which the identifier no longer exists
+		private void ReparseChangedIdentifier(List<string> identifierToReparse, int guidResultCountBefore, Dictionary<string, List<int>> identifiersToResolve, ChangedLineTracker changeTracker)
+		{
+			if (identifierToReparse.Count == 0)
+				return;
+
+			for (int i = guidResultCountBefore - 1; i >= 0; --i)
+			{
+				ParseResultLine guidResult = m_guidResultLines[i];
+				if (identifierToReparse.Contains(guidResult.Identifier))
+				{
+					ParseResultLine identifierDef = m_identifierDefinitionLines.Find((element) => element.Identifier == guidResult.Identifier);
+					if (!string.IsNullOrEmpty(identifierDef.Identifier))
+					{
+						if (identifierDef.GuidString != guidResult.GuidString)
+						{
+							guidResult.GuidString = identifierDef.GuidString;
+							m_guidResultLines[i] = guidResult;
+							changeTracker.UpdateLine(guidResult.LineNumber);
+						}
+					}
+					else
+					{
+						identifiersToResolve.GetOrCreate(guidResult.Identifier).Add(guidResult.LineNumber);
+						changeTracker.UpdateLine(guidResult.LineNumber);
+						m_guidResultLines.RemoveAtSwap(i);
+					}
+				}
+			}
+		}
+
+		// Parse all lines in the given snapshot span and search for guids and guid identifiers in them. Results are added to m_guidLineResults and m_identifierDefinitionLines
+		private void ParseLinesInSnapshotSpan(SnapshotSpan snapshotSpan, ChangedLineTracker changeTracker, out Dictionary<string, List<int>> identifiersToResolve)
+		{
+			identifiersToResolve = new Dictionary<string, List<int>>();
+			List<string> guidFieldIdentifiers = GameCodersToolkitPackage.DataLocationsConfig.GetGuidFieldIdentifiers();
+			ITextSnapshot snapshot = snapshotSpan.Snapshot;
+			int newStartLine = snapshotSpan.Start.GetContainingLine().LineNumber;
+			int newEndLine = snapshotSpan.End.GetContainingLine().LineNumber;
+
 			for (int lineToParse = newStartLine; lineToParse <= newEndLine; lineToParse++)
 			{
-				ITextSnapshotLine lineSnapshot = newSnapshot.GetLineFromLineNumber(lineToParse);
-				
+				ITextSnapshotLine lineSnapshot = snapshot.GetLineFromLineNumber(lineToParse);
+
 				string identifierWord = TextUtilFunctions.GetGuidIdentifierWordInLine(lineSnapshot, guidFieldIdentifiers);
 				string lineGuidString = TextUtilFunctions.FindGuidInLineSnapshot(lineSnapshot);
 				if (!string.IsNullOrEmpty(lineGuidString))
@@ -218,7 +295,7 @@ namespace GameCodersToolkitShared.DataReferenceFinderModule.CodeLensTagging
 					{
 						if (!string.IsNullOrWhiteSpace(identifierWord))
 						{
-							m_identifierDefinitionLines.Add(new ParseResultLine { GuidString =  lineGuidString, Identifier = identifierWord, LineNumber = lineToParse });
+							m_identifierDefinitionLines.Add(new ParseResultLine { GuidString = lineGuidString, Identifier = identifierWord, LineNumber = lineToParse });
 						}
 					}
 					m_guidResultLines.Add(new ParseResultLine { GuidString = lineGuidString, LineNumber = lineToParse, Identifier = identifierWord });
@@ -248,68 +325,38 @@ namespace GameCodersToolkitShared.DataReferenceFinderModule.CodeLensTagging
 					}
 				}
 			}
+		}
 
-			for (int i = guidResultCountBefore - 1; i >= 0; --i)
+		// Remove all current guid line results and identifier definitions for all lines in a change
+		private List<string> RemoveChangedLineEntries(ITextSnapshot oldSnapshot, Span oldSpan, ChangedLineTracker changeTracker)
+		{
+			SnapshotSpan oldSnapshotSpan = new SnapshotSpan(oldSnapshot, oldSpan);
+			int oldStartLine = oldSnapshotSpan.Start.GetContainingLine().LineNumber;
+			int oldEndLine = oldSnapshotSpan.End.GetContainingLine().LineNumber;
+
+			Span guidResultsToRemove = GetRangeToRemove(oldStartLine, oldEndLine, ref m_guidResultLines);
+			Span removedGuidLinesSpan = new Span();
+			if (!guidResultsToRemove.IsEmpty)
 			{
-				ParseResultLine guidResult = m_guidResultLines[i];
-				if (identifierToReparse.Contains(guidResult.Identifier))
-				{
-					ParseResultLine identifierDef = m_identifierDefinitionLines.Find((element) => element.Identifier == guidResult.Identifier); 
-					if (!string.IsNullOrEmpty(identifierDef.Identifier))
-					{
-						if (identifierDef.GuidString != guidResult.GuidString)
-						{
-							guidResult.GuidString = identifierDef.GuidString;
-							m_guidResultLines[i] = guidResult;
-							changeTracker.UpdateLine(guidResult.LineNumber);
-						}
-					}
-					else
-					{
-						identifiersToResolve.GetOrCreate(guidResult.Identifier).Add(guidResult.LineNumber);
-						m_guidResultLines.RemoveAtSwap(i);
-					}
-				}
+				removedGuidLinesSpan = new Span(m_guidResultLines[guidResultsToRemove.Start].LineNumber, m_guidResultLines[guidResultsToRemove.End - 1].LineNumber);
+				m_guidResultLines.RemoveRange(guidResultsToRemove.Start, guidResultsToRemove.Length);
 			}
 
-			// TODO resolve remaining guid identifiers with corresponding file
-
-			// Offset all lines we did not reparse but come after the new text span by the line delta of the change so our results stay attached to the correct line
-			if (lineDelta != 0)
+			Span identifierDefsToRemove = GetRangeToRemove(oldStartLine, oldEndLine, ref m_identifierDefinitionLines);
+			List<string> identifierToReparse = new List<string>();
+			for (int i = identifierDefsToRemove.Start; i < identifierDefsToRemove.End; ++i)
 			{
-				for (int i = 0; i < guidResultCountBefore; ++i)
-				{
-					if (m_guidResultLines[i].LineNumber >= newStartLine)
-					{
-						ParseResultLine parseResultLine = m_guidResultLines[i];
-						changeTracker.UpdateLine(parseResultLine.LineNumber);
+				identifierToReparse.Add(m_identifierDefinitionLines[i].Identifier);
+			}
+			m_identifierDefinitionLines.RemoveRange(identifierDefsToRemove.Start, identifierDefsToRemove.Length);
 
-						parseResultLine.LineNumber += lineDelta;
-						m_guidResultLines[i] = parseResultLine;
-
-						changeTracker.UpdateLine(parseResultLine.LineNumber);
-					}
-				}
+			if (!removedGuidLinesSpan.IsEmpty)
+			{
+				changeTracker.UpdateLine(removedGuidLinesSpan.Start);
+				changeTracker.UpdateLine(removedGuidLinesSpan.End);
 			}
 
-			// Sort all results by their line number again.
-			ParseResultLineComparer resultLineComparer = new ParseResultLineComparer();
-			m_guidResultLines.Sort(resultLineComparer);
-			m_identifierDefinitionLines.Sort(resultLineComparer);
-
-			stopwatch.Stop();
-			GameCodersToolkitPackage.ExtensionOutput?.WriteLine($"Parsing Text Buffer {m_textBuffer.GetFileName()} took {stopwatch.ElapsedMilliseconds}ms");
-
-			m_currentSnapshot = newSnapshot;
-			// We changed atleast one line. Make sure to notify about it
-			if (changeTracker.FirstChangedLine != int.MaxValue)
-			{
-				ITextSnapshotLine firstSnapshotLine = newSnapshot.GetLineFromLineNumber(changeTracker.FirstChangedLine);
-				ITextSnapshotLine lastSnapshotLine = newSnapshot.GetLineFromLineNumber(changeTracker.LastChangedLine);
-				TagsChanged?.Invoke(this,
-					new SnapshotSpanEventArgs(
-						new SnapshotSpan(newSnapshot, firstSnapshotLine.Start, lastSnapshotLine.End)));
-			}
+			return identifierToReparse;
 		}
 
 		// Returns the index based span to remove from the given lines. The first index is inclusive while the second is exclusive
@@ -354,33 +401,6 @@ namespace GameCodersToolkitShared.DataReferenceFinderModule.CodeLensTagging
 			}
 
 			return new Span();
-		}
-
-		class ChangedLineTracker()
-		{
-			public void UpdateLine(int lineNumber)
-			{
-				FirstChangedLine = Math.Min(FirstChangedLine, lineNumber);
-				LastChangedLine = Math.Max(LastChangedLine, lineNumber);
-			}
-
-			public int FirstChangedLine { get; private set; } = int.MaxValue;
-			public int LastChangedLine { get; private set; } = -1;
-		}
-
-		struct ParseResultLine
-		{
-			public int LineNumber;
-			public string GuidString;
-			public string Identifier;
-		}
-
-		class ParseResultLineComparer : IComparer<ParseResultLine>
-		{
-			public int Compare(ParseResultLine a, ParseResultLine b)
-			{
-				return a.LineNumber - b.LineNumber;
-			}
 		}
 
 		List<ParseResultLine> m_guidResultLines = new List<ParseResultLine>();
